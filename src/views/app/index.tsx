ddd43f262a9860247e5f462662fcdbf26f539954
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-10-20 13:58:40 +0300

    feat: implement TaskItem

--- src/views/app/index.tsx
+++ src/views/app/index.tsx
@@ -1,12 +1,24 @@
 import styles from './index.module.scss';
+import { useEffect } from 'react';
 import { AddTask } from '../components/AddTask';
 import { useToDoStore } from '../../data/store/useToDoStore';
+import { TaskItem } from '../components/TaskItem';
 
 export const App = () => {
+  const tasks = useToDoStore((state) => state.tasks);
   const createTask = useToDoStore((state) => state.createTask);
+  const updateTask = useToDoStore((state) => state.updateTask);
+  const removeTask = useToDoStore((state) => state.removeTask);
+
+  useEffect(() => {
+    createTask('');
+  }, [createTask]);
+
+
+
   return (
     <div className={styles.app}>
-      <h1 className={styles.appTitle}>Todo Task App</h1>
+      <h1 className={styles.appTitle}>TO DO OR NOT TO DO APP</h1>
       <section className={styles.appSection}>
         <AddTask
           onAdd={(title: string) => {
@@ -17,7 +29,21 @@ export const App = () => {
         />
       </section>
 
-      <section className={styles.appSection}></section>
+      <section className={styles.appSection}>
+        {!tasks.length && (
+          <p className={styles.appSectionText}>There are no tasks.</p>
+        )}
+        {tasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            id={task.id}
+            title={task.title}
+            onDeleted={removeTask}
+            onCompleted={removeTask}
+            onEdit={updateTask}
+          />
+        ))}
+      </section>
     </div>
   );
 };
---
+++ src/views/components/TaskItem/index.tsx
@@ -0,0 +1,73 @@
+import styles from './index.module.scss';
+import { useState } from 'react';
+
+interface TaskItemProps {
+  id: string;
+  title: string;
+  onCompleted: (id: string) => void;
+  onDeleted: (id: string) => void;
+  onEdit: (id: string, value: string) => void;
+}
+
+export const TaskItem: React.FC<TaskItemProps> = ({
+  id,
+  title,
+  onCompleted,
+  onDeleted,
+  onEdit,
+}) => {
+  const [checked, setChecked] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [value, setValue] = useState(title);
+
+  return (
+    <div className={styles.inputTaskItem}>
+      <label className={styles.inputTaskItemLabel}>
+        <input
+          type="checkbox"
+          disabled={edit}
+          checked={checked}
+          className={styles.inputTaskItemCheckBox}
+          onChange={(e) => {
+            setChecked(e.target.checked);
+            if (e.target.checked) {
+              onCompleted(id);
+            }
+          }}
+        />
+        {edit ? (
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className={styles.inputTaskItemTitleEdit}
+          />
+        ) : (
+          <h3 className={styles.inputTaskItemTitle}>{title}</h3>
+        )}
+      </label>
+
+      {edit ? (
+        <button
+          aria-label="Save"
+          className={styles.saveButton}
+          onClick={() => {
+            onEdit(id, value);
+            setEdit(false);
+          }}
+        />
+      ) : (
+        <button
+          aria-label="edit"
+          onClick={() => setEdit(true)}
+          className={styles.editButton}
+        />
+      )}
+
+      <button
+        aria-label="delete"
+        onClick={() => onDeleted(id)}
+        className={styles.deleteButton}
+      />
+    </div>
+  );
+};
