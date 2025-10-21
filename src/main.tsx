9e0e34573a9f25a4b0bf778697df5d87a960ffe7
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-10-21 18:36:34 +0300

    style: add scss variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './views/app';
-import './styles/common.scss';
+import './styles/global.scss';
 import './styles/reset.scss';
 
 createRoot(document.getElementById('root')!).render(
--- src/views/components/AddTask/index.tsx
+++ src/views/components/AddTask/index.tsx
@@ -37,7 +37,7 @@ export const AddTask = ({ onAdd }: AddTaskProps) => {
         placeholder="Add a task"
         aria-label="Add a task"
       />
-      <button onClick={handleAddTask} type="submit">
+      <button className={styles.addTaskButton} onClick={handleAddTask} type="submit">
         Add
       </button>
     </div>
