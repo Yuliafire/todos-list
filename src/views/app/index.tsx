3133ed6e12da4462d11f53e3d59dad852c543eb6
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-10-21 12:28:07 +0300

    feat: add activeTask count function

--- src/views/app/index.tsx
+++ src/views/app/index.tsx
@@ -3,16 +3,30 @@ import { AddTask } from '../components/AddTask';
 import { useToDoStore } from '../../data/store/useToDoStore';
 import { TaskItem } from '../components/TaskItem';
 
+
 export const App = () => {
   const tasks = useToDoStore((state) => state.tasks);
   const createTask = useToDoStore((state) => state.createTask);
   const updateTask = useToDoStore((state) => state.updateTask);
   const removeTask = useToDoStore((state) => state.removeTask);
   const toggleTask = useToDoStore((state) => state.toggleTask);
+  // const activeTasksCount = tasks.filter((task) => !task.completed).length;
+
+  const activeTasksCount = useToDoStore(
+    (state) => state.tasks.filter((task) => !task.completed).length
+  );
 
   return (
     <div className={styles.app}>
       <h1 className={styles.appTitle}>TO DO OR NOT TO DO APP</h1>
+
+      {tasks.length > 0 && (
+        <div className={styles.tasksInfo}>
+          <p className={styles.activeTasksCount}>
+            Осталось задач: {activeTasksCount}
+          </p>
+        </div>
+      )}
       <section className={styles.appSection}>
         <AddTask
           onAdd={(title: string) => {
